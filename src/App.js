fa5fb14ee6c6dab66ea4f9248a88f97b07751745
Degisew <jegisew21@gmail.com>
2022-11-30 01:35:44 +0300

    wrap a component with provider and router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import Details from "./components/Details/Details";
+import Home from "./components/Home/Home";
 function App() {
   return (
     <div className="App">
-      Hello
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/details" element={ <Details />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
+  </Provider>
 );
