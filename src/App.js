c42515b46d5a50e787fd58aa60935c016c42431e
Degisew <jegisew21@gmail.com>
2022-12-02 00:59:51 +0300

    render appbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
+import NavBar from "./components/Appbar/NavBar";
 import Details from "./components/Details/Details";
 import Home from "./components/Home/Home";
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <div className="spacer-div" />
       <Routes>
-        <Route path="/" element={ <Home />} />
-        <Route path="/details" element={ <Details />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
       </Routes>
     </div>
   );
